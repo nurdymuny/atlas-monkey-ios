2efadb3ea1828030d635278c68cbbfb89b313caf
nurdymuny <brian@brianrusseldavis.com>
2016-01-25 20:05:40 +0530

    Convert code in xcode7.2 nad Implemented th logic for finding th nearest iBeacon usding AprilSDK

---
+++ appcode/monkey-bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  monkey-bridging-Header.h
+//  Concert monkey
+//
+//  Created by attmac101 on 25/01/16.
+//  Copyright © 2016 nurdymuny. All rights reserved.
+//
+
+#ifndef monkey_bridging_Header_h
+#define monkey_bridging_Header_h
+
+#import "AprilBeaconSDK.h"
+#import "ABTransmitters.h"
+
+#endif /* monkey_bridging_Header_h */
